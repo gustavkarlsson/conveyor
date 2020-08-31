f7beede00c0c62810bc4eb6ecfb1849a1dec6ae8
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-08-31 18:34:13 +0200

    Add custom action classes

--- lib/src/main/kotlin/se/gustavkarlsson/cokrate/Action.kt
+++ lib/src/main/kotlin/se/gustavkarlsson/cokrate/Action.kt
@@ -1,5 +1,33 @@
 package se.gustavkarlsson.cokrate
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+
 public interface Action<State : Any> {
     public suspend fun execute(issue: suspend (Command<State>) -> Unit)
+}
+
+public class SingleAction<State : Any>(
+    private val block: suspend () -> Command<State>
+) : Action<State> {
+    override suspend fun execute(issue: suspend (Command<State>) -> Unit) {
+        val command = block()
+        issue(command)
+    }
+}
+
+public class MultiAction<State : Any>(
+    private val collect: suspend (issue: suspend (Command<State>) -> Unit) -> Unit
+) : Action<State> {
+    override suspend fun execute(issue: suspend (Command<State>) -> Unit) {
+        collect(issue)
+    }
+}
+
+public class FlowAction<State : Any>(
+    private val flow: Flow<Command<State>>
+) : Action<State> {
+    override suspend fun execute(issue: suspend (Command<State>) -> Unit) {
+        flow.collect(issue)
+    }
 }
