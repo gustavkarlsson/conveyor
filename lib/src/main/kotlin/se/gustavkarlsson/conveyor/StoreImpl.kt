e5bf27d419768777698843b5565dcaf86c8e435e
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-09-03 21:43:17 +0200

    Improved CommandProcessor

--- lib/src/main/kotlin/se/gustavkarlsson/conveyor/StoreImpl.kt
+++ lib/src/main/kotlin/se/gustavkarlsson/conveyor/StoreImpl.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ConflatedBroadcastChannel
-import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
@@ -32,28 +31,16 @@ internal class StoreImpl<State>(
             else -> Status.Cancelled
         }
 
-    // TODO encapsulate command stuff into new class
-    private val commands = Channel<Command<State>>(commandBufferSize)
-
-    private val commandIssuer = ChannelCommandIssuer(commands)
-
     private val stateHolder = StateHolder(initialState)
+
+    private val commandProcessor = CommandProcessor(commandBufferSize, stateHolder::get, stateHolder::set)
 
     override val state = stateHolder.flow
 
-    override val currentState get() = stateHolder.current
+    override val currentState get() = stateHolder.get()
 
     // TODO encapsulate action stuff into new class
     private val initialActions = ArrayDeque(initialActions.toList())
-
-    private val actionExecutor = ActionExecutor(commandIssuer)
-
-    init {
-        require(commandBufferSize > 0) {
-            "commandBufferSize must be positive. Was: $commandBufferSize"
-        }
-        stateHolder.set(initialState)
-    }
 
     @Synchronized
     override fun start(scope: CoroutineScope): Job {
@@ -61,20 +48,15 @@ internal class StoreImpl<State>(
         check(currentStatus == Status.NotYetStarted) {
             "Cannot start store when it is $currentStatus"
         }
+        val actionExecutor = ActionExecutor(commandProcessor)
         val job = scope.launch {
             initialActions.removeAll { action ->
                 launch { actionExecutor.execute(action) }
                 true
             }
-            commands
-                .consumeEach { command ->
-                    val oldState = stateHolder.current
-                    val (newState, actions) = command.reduce(oldState)
-                    stateHolder.set(newState)
-                    for (action in actions) {
-                        launch { actionExecutor.execute(action) }
-                    }
-                }
+            commandProcessor.process {
+                launch { actionExecutor.execute(it) }
+            }
         }
         job.invokeOnCompletion { stateHolder.close(it) }
         this.job = job
@@ -86,35 +68,55 @@ internal class StoreImpl<State>(
         check(currentStatus == Status.Active) {
             "Cannot issue command while store is $currentStatus"
         }
-        commandIssuer.issue(command)
+        commandProcessor.issue(command)
     }
 }
 
 private enum class Status { NotYetStarted, Active, Cancelled }
 
-private class ChannelCommandIssuer<State>(
-    private val channel: SendChannel<Command<State>>,
+@ExperimentalCoroutinesApi
+private class CommandProcessor<State>(
+    bufferSize: Int,
+    private val getState: () -> State,
+    private val setState: (State) -> Unit,
 ) : CommandIssuer<State> {
+    init {
+        require(bufferSize > 0) {
+            "bufferSize must be positive. Was: $bufferSize"
+        }
+    }
+
+    private val commands = Channel<Command<State>>(bufferSize)
+
     override suspend fun issue(command: Command<State>) {
-        channel.send(command)
+        commands.send(command)
+    }
+
+    suspend fun process(onAction: suspend (Action<State>) -> Unit) {
+        commands.consumeEach { command ->
+            val oldState = getState()
+            val (newState, actions) = command.reduce(oldState)
+            setState(newState)
+            for (action in actions) {
+                onAction(action)
+            }
+        }
     }
 }
 
 @FlowPreview
 @ExperimentalCoroutinesApi
-private class StateHolder<State>(
-    initialState: State,
-) {
+private class StateHolder<State>(initialState: State) {
     private val channel = ConflatedBroadcastChannel(initialState)
 
-    val current: State get() = channel.value
-
-    val flow: Flow<State> = channel.asFlow()
-        .distinctUntilChanged { old, new -> old === new }
+    fun get(): State = channel.value
 
     fun set(state: State) {
         check(channel.offer(state)) { "Failed to set state, channel over capacity" }
     }
+
+    val flow: Flow<State> = channel.asFlow()
+        .distinctUntilChanged { old, new -> old === new }
 
     fun close(cause: Throwable?) {
         channel.close(cause)
