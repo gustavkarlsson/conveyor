0a8163f34a247fb2ea5b2a028d0dfd84ff8ab9e5
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-09-06 21:59:02 +0200

    Remove unused import

--- rx2/src/main/kotlin/se/gustavkarlsson/conveyor/rx2/ToRxStore.kt
+++ rx2/src/main/kotlin/se/gustavkarlsson/conveyor/rx2/ToRxStore.kt
@@ -3,7 +3,6 @@ package se.gustavkarlsson.conveyor.rx2
 import se.gustavkarlsson.conveyor.Store
 import se.gustavkarlsson.conveyor.rx2.internal.RxStoreImpl
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 
 public fun <State : Any> Store<State>.toRxStore(
     context: CoroutineContext? = null,
