3ef0644cfbb72b5fefbacc35228f0f55ab4e9770
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-10-08 18:32:51 +0200

    Better StoreStoppedException message

--- core/src/main/kotlin/se/gustavkarlsson/conveyor/StoreStoppedException.kt
+++ core/src/main/kotlin/se/gustavkarlsson/conveyor/StoreStoppedException.kt
@@ -2,4 +2,12 @@ package se.gustavkarlsson.conveyor
 
 public class StoreStoppedException(
     public val cancellationReason: Throwable?,
-) : IllegalStateException("Store has been stopped")
+) : IllegalStateException(createMessage(cancellationReason))
+
+private fun createMessage(reason: Throwable?) =
+    if (reason == null) {
+        "Store has been stopped"
+    }
+    else {
+        "Store has been stopped due to cancellation"
+    }
