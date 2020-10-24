8749690a9f444748361ec4a6de5c3df947191c3f
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-10-24 15:35:31 +0200

    Re-introduce state watching

--- core/src/main/kotlin/se/gustavkarlsson/conveyor/BuildStore.kt
+++ core/src/main/kotlin/se/gustavkarlsson/conveyor/BuildStore.kt
@@ -3,6 +3,7 @@ package se.gustavkarlsson.conveyor
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onEach
 import se.gustavkarlsson.conveyor.internal.LiveActionsManager
@@ -17,15 +18,17 @@ public fun <State> buildStore(
     initialState: State,
     startActions: Iterable<Action<State>> = emptyList(),
     liveActions: Iterable<Action<State>> = emptyList(),
-    stateWatchers: Iterable<Watcher<State>> = emptyList(), // FIXME what do do with watchers?
+    stateWatchers: Iterable<Watcher<State>> = emptyList(),
     actionWatchers: Iterable<Watcher<Action<State>>> = emptyList(),
     plugins: Iterable<Plugin<State>> = emptyList(),
 ): Store<State> {
     val actionTransformers: Iterable<Transformer<Action<State>>> =
-        actionWatchers.map { it.toTransformer() }.asIterable()
+        actionWatchers.map { it.toTransformer() }
+    val watcherActions: Iterable<Action<State>> =
+        stateWatchers.map { it.toAction() }
 
     val overriddenInitialState = initialState.override(plugins) { overrideInitialState(it) }
-    val overriddenStartActions = startActions.override(plugins) { overrideStartActions(it) }
+    val overriddenStartActions = (startActions + watcherActions).asIterable().override(plugins) { overrideStartActions(it) }
     val overriddenLiveActions = liveActions.override(plugins) { overrideLiveActions(it) }
     val overriddenActionTransformers = actionTransformers.override(plugins) { overrideActionTransformers(it) }
 
@@ -52,6 +55,16 @@ private fun <T> Watcher<T>.toTransformer() = WatchingTransformer(this)
 
 private class WatchingTransformer<T>(private val watcher: Watcher<T>) : Transformer<T> {
     override fun transform(flow: Flow<T>): Flow<T> = flow.onEach { watcher.watch(it) }
+}
+
+private fun <T> Watcher<T>.toAction() = WatchingAction(this)
+
+private class WatchingAction<T>(private val watcher: Watcher<T>) : Action<T> {
+    override suspend fun execute(stateAccess: StateAccess<T>) {
+        stateAccess.flow.collect { state ->
+            watcher.watch(state)
+        }
+    }
 }
 
 private fun <State, T> T.override(
