7b787317fbf99e6c4f8c9eb7c7b2924e6ca426e7
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-10-31 11:41:29 +0100

    Improve tests for ManualActionsManager

--- core/src/test/kotlin/se/gustavkarlsson/conveyor/internal/ManualActionsManagerTest.kt
+++ core/src/test/kotlin/se/gustavkarlsson/conveyor/internal/ManualActionsManagerTest.kt
@@ -1,56 +1,57 @@
 package se.gustavkarlsson.conveyor.internal
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.toCollection
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import se.gustavkarlsson.conveyor.Action
-import se.gustavkarlsson.conveyor.action
+import se.gustavkarlsson.conveyor.test.NullAction
 import se.gustavkarlsson.conveyor.test.memoizedTestCoroutineScope
-import se.gustavkarlsson.conveyor.test.runBlockingTest
 import strikt.api.expectThat
 import strikt.api.expectThrows
 import strikt.assertions.containsExactly
 
 object ManualActionsManagerTest : Spek({
     val scope by memoizedTestCoroutineScope()
-    val action = action<String> {}
+    val action = NullAction<String>()
+
     describe("A ManualActionsManager") {
         val subject by memoized { ManualActionsManager<String>() }
 
         it("suspends on collecting actionFlow") {
             expectSuspends {
-                subject.actionFlow.collect {}
+                subject.actionFlow.collect()
             }
         }
 
         describe("that is collecting actionFlow") {
-            val executedActions by memoized {
+            val collectedActions by memoized {
                 mutableListOf<Action<String>>()
             }
             lateinit var collectingJob: Job
             beforeEachTest {
                 collectingJob = scope.launch {
-                    subject.actionFlow.collect { executedActions += it }
+                    subject.actionFlow.toCollection(collectedActions)
                 }
             }
             afterEachTest {
                 collectingJob.cancel("Test ended")
             }
 
-            it("issued action executes") {
+            it("issued action collected") {
                 subject.issue(action)
-                expectThat(executedActions).containsExactly(action)
+                expectThat(collectedActions).containsExactly(action)
             }
             it("throws if collecting actionFlow again") {
                 expectThrows<IllegalStateException> {
-                    runBlockingTest {
-                        subject.actionFlow.collect {}
-                    }
+                    subject.actionFlow.collect()
                 }
-            }
-            it("actions are run in parallel") {
-
             }
         }
 
@@ -60,13 +61,13 @@ object ManualActionsManagerTest : Spek({
             }
 
             it("throws exception when action is issued") {
-                expectThrows<IllegalStateException> {
+                expectThrows<CancellationException> {
                     subject.issue(action)
                 }
             }
-            it("does not suspend on collecting actionFlow") {
-                runBlockingTest {
-                    subject.actionFlow.collect {}
+            it("throws exception when collecting actionFlow") {
+                expectThrows<CancellationException> {
+                    subject.actionFlow.collect()
                 }
             }
             it("can be cancelled again") {
@@ -77,11 +78,9 @@ object ManualActionsManagerTest : Spek({
 })
 
 private fun expectSuspends(block: suspend () -> Unit) {
-    runBlockingTest {
-        expectThrows<TimeoutCancellationException> {
-            withTimeout(100) {
-                block()
-            }
+    expectThrows<TimeoutCancellationException> {
+        withTimeout(100) {
+            block()
         }
     }
 }
