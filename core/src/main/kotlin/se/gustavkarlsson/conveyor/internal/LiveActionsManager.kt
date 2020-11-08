38b04c137a9ed32ecaa2f90a21fddbdcde19b4cd
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-11-08 13:07:48 +0100

    Simplify LiveActionsManager slightly

--- core/src/main/kotlin/se/gustavkarlsson/conveyor/internal/LiveActionsManager.kt
+++ core/src/main/kotlin/se/gustavkarlsson/conveyor/internal/LiveActionsManager.kt
@@ -2,17 +2,20 @@ package se.gustavkarlsson.conveyor.internal
 
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import se.gustavkarlsson.conveyor.Action
 import se.gustavkarlsson.conveyor.StateAccess
 
+@FlowPreview
 @ExperimentalCoroutinesApi
 internal class LiveActionsManager<State>(
     actions: Iterable<Action<State>>,
@@ -21,16 +24,17 @@ internal class LiveActionsManager<State>(
     private val cancellationChannel = Channel<Unit>(Channel.CONFLATED)
         .apply { offerOrThrow(Unit) }
 
-    private val toggleFlow = combine(
-        subscriptionCount.distinctUntilChanged(),
-        cancellationChannel.consumeAsFlow()
-    ) { count, _ ->
-        when {
-            count < 0 -> error("count may not be negative")
-            count == 0 -> Toggle.Disable
-            else -> Toggle.Enable
+    private val toggleFlow = cancellationChannel.consumeAsFlow()
+        .flatMapConcat {
+            subscriptionCount.distinctUntilChanged()
         }
-    }
+        .map { count ->
+            when {
+                count < 0 -> error("count may not be negative")
+                count == 0 -> Toggle.Disable
+                else -> Toggle.Enable
+            }
+        }
 
     private var actions: Iterable<Action<State>>? = actions.toList()
 
