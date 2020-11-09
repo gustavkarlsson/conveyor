ac9cdb7ab423c4b403897cc454a41b7924b54878
Gustav Karlsson <gustav.karlsson@ioki.com>
2020-11-09 21:23:55 +0100

    Extract ActionIssuer

--- core/src/main/kotlin/se/gustavkarlsson/conveyor/Store.kt
+++ core/src/main/kotlin/se/gustavkarlsson/conveyor/Store.kt
@@ -4,15 +4,10 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.StateFlow
 
-public interface Store<State> {
+public interface Store<State> : ActionIssuer<State> {
     public val state: StateFlow<State>
     public fun start(scope: CoroutineScope): Job
-    public fun issue(action: Action<State>)
 }
 
 public fun <State> CoroutineScope.start(store: Store<State>): Job =
     store.start(this)
-
-public fun <State> Store<State>.issue(
-    block: suspend (state: UpdatableStateFlow<State>) -> Unit,
-): Unit = issue(action(block))
