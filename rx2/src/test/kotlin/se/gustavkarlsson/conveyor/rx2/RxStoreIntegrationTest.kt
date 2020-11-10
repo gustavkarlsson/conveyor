cafe77a534a7972e83189817606fd5a4d720bd66
Gustav Karlsson <gustav.karlsson@ioki.com>
2020-11-10 20:17:22 +0100

    Update test

--- rx2/src/test/kotlin/se/gustavkarlsson/conveyor/rx2/RxStoreIntegrationTest.kt
+++ rx2/src/test/kotlin/se/gustavkarlsson/conveyor/rx2/RxStoreIntegrationTest.kt
@@ -5,12 +5,16 @@ import io.reactivex.disposables.Disposable
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import se.gustavkarlsson.conveyor.buildStore
+import se.gustavkarlsson.conveyor.rx2.test.memoizedTestCoroutineScope
 
 object RxStoreIntegrationTest : Spek({
+    val scope by memoizedTestCoroutineScope()
+
     describe("A started RxStore") {
         val subject by memoized { buildStore(0).toRxStore() }
-        var disposable : Disposable? = null
-        beforeEachTest { disposable = subject.start() }
+
+        var disposable: Disposable? = null
+        beforeEachTest { disposable = subject.start(scope) }
         afterEachTest { disposable?.dispose() }
 
         it("Executes sequentially") {
@@ -18,7 +22,7 @@ object RxStoreIntegrationTest : Spek({
             subject.issue { state ->
                 state.update { Single.just(1) }.ignoreElement()
             }
-            testSubscriber.assertValue(1)
+            testSubscriber.assertValues(0, 1)
         }
     }
 })
