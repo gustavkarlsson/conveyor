18e1e1b4b1513f7f6117509be1bb7032f3299251
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-11-13 17:29:02 +0100

    Fix multiple kotlin plugins with explicit version

--- testing/build.gradle.kts
+++ testing/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version Versions.kotlin
+    kotlin("jvm")
     id("io.gitlab.arturbosch.detekt") version Versions.detekt
 }
 
