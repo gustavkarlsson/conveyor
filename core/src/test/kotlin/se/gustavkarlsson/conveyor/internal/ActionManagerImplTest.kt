18b2fa252eee455aa3a169a4b7effa0201f6f444
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-11-13 15:44:40 +0100

    Add more tests to ActionManagerImpl

--- core/src/test/kotlin/se/gustavkarlsson/conveyor/internal/ActionManagerImplTest.kt
+++ core/src/test/kotlin/se/gustavkarlsson/conveyor/internal/ActionManagerImplTest.kt
@@ -2,31 +2,61 @@ package se.gustavkarlsson.conveyor.internal
 
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withTimeout
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import se.gustavkarlsson.conveyor.action
 import se.gustavkarlsson.conveyor.test.IncrementStateAction
 import se.gustavkarlsson.conveyor.test.memoizedTestCoroutineScope
+import se.gustavkarlsson.conveyor.test.runBlockingTest
+import strikt.api.expectThat
 import strikt.api.expectThrows
+import strikt.assertions.isEqualTo
 
-// TODO Add tests for actions flow
 object ActionManagerImplTest : Spek({
-    val scope by memoizedTestCoroutineScope()
-    val state by memoized { UpdatableStateFlowImpl(0) }
-    val incrementStateAction = IncrementStateAction()
+    val action = action<Int> {}
 
     describe("A ActionManagerImpl") {
         val subject by memoized { ActionManagerImpl<Int>() }
 
+        it("actions suspends waiting for first item") {
+            expectSuspends {
+                subject.actions.first()
+            }
+        }
+        it("actions emits action issued after subscribing") {
+            val result = runBlockingTest {
+                val deferred = async { subject.actions.first() }
+                subject.issue(action)
+                deferred.await()
+            }
+            expectThat(result).isEqualTo(action)
+        }
+        it("actions emits action issued before subscribing") {
+            val result = runBlockingTest {
+                subject.issue(action)
+                subject.actions.first()
+            }
+            expectThat(result).isEqualTo(action)
+        }
+
         describe("that was cancelled") {
+            val exception = CancellationException("Purposefully cancelled")
             beforeEachTest {
-                subject.cancel(null)
+                subject.cancel(exception)
             }
 
+            it("actions emits error") {
+                expectThrows<CancellationException> {
+                    subject.actions.first()
+                }.isEqualTo(exception)
+            }
             it("throws exception when action is issued") {
                 expectThrows<CancellationException> {
-                    subject.issue(incrementStateAction)
-                }
+                    subject.issue(action)
+                }.isEqualTo(exception)
             }
             it("can be cancelled again") {
                 subject.cancel(null)
