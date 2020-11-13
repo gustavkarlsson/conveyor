3c7001d64b46ece0debda2d21dc122d925e4f2a7
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-11-13 18:32:10 +0100

    Fix missing plugin repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,15 @@
 rootProject.name = "conveyor"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
 include(
     "core",
     "rx2",
+    "demo",
     "testing"
 )
