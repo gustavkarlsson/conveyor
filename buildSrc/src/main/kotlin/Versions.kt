9c95036f455b59c5517aea00e199b89f956841a9
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-11-28 17:09:02 +0100

    Update versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,17 +1,17 @@
 object Versions {
     // Config
     const val java = "1.8"
-    const val kotlin = "1.4.10"
+    const val kotlin = "1.4.20"
     const val detekt = "1.14.2"
-    const val dokka = "1.4.10"
+    const val dokka = "1.4.10.2"
     const val jacoco = "0.8.6"
 
     // Dependencies
-    const val kotlinxCoroutines = "1.4.1"
+    const val kotlinxCoroutines = "1.4.2"
     const val rxJava2 = "2.2.20"
 
     // Test
     const val junit5 = "5.7.0"
-    const val spek = "2.0.13"
+    const val spek = "2.0.14"
     const val strikt = "0.28.0"
 }
