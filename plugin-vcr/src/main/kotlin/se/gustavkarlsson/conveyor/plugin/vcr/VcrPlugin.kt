9cbb73e85922c4522b90e7c869ad27b80e8d8727
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-11-30 00:58:22 +0100

    Fix compilation errors

--- plugin-vcr/src/main/kotlin/se/gustavkarlsson/conveyor/plugin/vcr/VcrPlugin.kt
+++ plugin-vcr/src/main/kotlin/se/gustavkarlsson/conveyor/plugin/vcr/VcrPlugin.kt
@@ -1,24 +1,28 @@
 package se.gustavkarlsson.conveyor.plugin.vcr
 
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combineTransform
 import se.gustavkarlsson.conveyor.Action
 import se.gustavkarlsson.conveyor.Plugin
-import se.gustavkarlsson.conveyor.Transformer
 import se.gustavkarlsson.conveyor.plugin.vcr.internal.Mode
 import se.gustavkarlsson.conveyor.plugin.vcr.internal.PlaybackAction
-import se.gustavkarlsson.conveyor.plugin.vcr.internal.PlaybackActionFilter
 import se.gustavkarlsson.conveyor.plugin.vcr.internal.RecordAction
 
 public class VcrPlugin<State> : Vcr<State>, Plugin<State> {
     private val mode = MutableStateFlow<Mode<State>>(Mode.Idle) // FIXME Rename mode?
 
-    override fun overrideStartActions(
-        startActions: Iterable<Action<State>>,
-    ): Iterable<Action<State>> = startActions + RecordAction(mode) + PlaybackAction(mode)
+    override fun addStartActions(): Iterable<Action<State>> = listOf(RecordAction(mode), PlaybackAction(mode))
 
-    override fun overrideActionTransformers(
-        actionTransformers: Iterable<Transformer<Action<State>>>,
-    ): Iterable<Transformer<Action<State>>> = actionTransformers + PlaybackActionFilter(mode)
+    override fun transformActions(actions: Flow<Action<State>>): Flow<Action<State>> =
+        combineTransform(actions, mode) { action, mode ->
+            when (mode) {
+                Mode.Idle, is Mode.Recording -> emit(action)
+                is Mode.Playing -> when (action) {
+                    is PlaybackAction, is RecordAction -> emit(action)
+                }
+            }
+        }
 
     override fun play(tape: ReadableTape<State>, bufferSize: Int) {
         val reading = tape.openForReading()
