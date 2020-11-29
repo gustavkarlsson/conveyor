94e317b5dcfb5fa0785fe17432d67c602e8fdcf8
Gustav Karlsson <gustav.karlsson@gmail.com>
2020-11-29 22:18:40 +0100

    Add documentation to core module

--- core/src/main/kotlin/se/gustavkarlsson/conveyor/Plugin.kt
+++ core/src/main/kotlin/se/gustavkarlsson/conveyor/Plugin.kt
@@ -1,18 +1,37 @@
 package se.gustavkarlsson.conveyor
 
+/**
+ * Enables changing the behavior of a [Store] by modifying how it's created and how it processes actions and the state.
+ */
 public interface Plugin<State> {
+    /**
+     * Override the initial state of the [Store].
+     */
     public fun overrideInitialState(
         initialState: State,
     ): State = initialState
 
+    /**
+     * Override the start actions of the [Store].
+     */
     public fun overrideStartActions(
         startActions: Iterable<Action<State>>,
     ): Iterable<Action<State>> = startActions
 
+    /**
+     * Override the action transformers of the [Store].
+     * Action transformers change the flow of actions before they are processed.
+     */
     public fun overrideActionTransformers(
         actionTransformers: Iterable<Transformer<Action<State>>>,
     ): Iterable<Transformer<Action<State>>> = actionTransformers
 
+    /**
+     * Override the state transformers of the [Store].
+     * State transformers change the "external" state exposed by [Store.state].
+     *
+     * *Note: State transformers do NOT change the "internal" state that actions operate on.*
+     */
     public fun overrideStateTransformers(
         stateTransformers: Iterable<Transformer<State>>,
     ): Iterable<Transformer<State>> = stateTransformers
--- core/src/main/kotlin/se/gustavkarlsson/conveyor/Store.kt
+++ core/src/main/kotlin/se/gustavkarlsson/conveyor/Store.kt
@@ -3,17 +3,43 @@ package se.gustavkarlsson.conveyor
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.StateFlow
-import se.gustavkarlsson.conveyor.internal.ActionIssuerImpl
 import se.gustavkarlsson.conveyor.internal.ActionExecutor
+import se.gustavkarlsson.conveyor.internal.ActionIssuerImpl
 import se.gustavkarlsson.conveyor.internal.StateTransformer
 import se.gustavkarlsson.conveyor.internal.StoreImpl
 import se.gustavkarlsson.conveyor.internal.UpdatableStateFlowImpl
 
+/**
+ * A predictable state container. The state can be read through [state]
+ * and updated by issuing actions with [issue].
+ *
+ * The lifecycle of a store is simple. Initially the store is idle and will have it's initial state.
+ * To make anything happen, the store must be started with [start].
+ * This will run any "start actions" provided and also allows new actions to be issued.
+ * The store is stopped when it's scope or job is cancelled. This cancels any running actions.
+ */
 public interface Store<State> : ActionIssuer<State> {
+    /**
+     * The state of the store. Always available regardless of which lifecycle stage the store is in.
+     */
     public val state: StateFlow<State>
+
+    /**
+     * Starts the store in the provided scope.
+     * This will run any "start actions" provided and also allows new actions to be issued.
+     *
+     * A store can only be started once.
+     *
+     * The store will be stopped when its scope or the returned [Job] is cancelled.
+     */
     public fun start(scope: CoroutineScope): Job
 }
 
+/**
+ * Creates a store with the given initial state.
+ * Any start action provided will run automatically when the store is started.
+ * Additionally, plugins can be provided to modify the Store's behavior even further.
+ */
 @Suppress("FunctionName")
 public fun <State> Store(
     initialState: State,
@@ -47,6 +73,14 @@ public fun <State> Store(
     )
 }
 
+/**
+ * Starts the provided store in this scope.
+ * This will run any "start actions" provided and also allows new actions to be issued.
+ *
+ * A store can only be started once.
+ *
+ * The store will be stopped when its scope or the returned [Job] is cancelled.
+ */
 public fun <State> CoroutineScope.start(store: Store<State>): Job =
     store.start(this)
 
