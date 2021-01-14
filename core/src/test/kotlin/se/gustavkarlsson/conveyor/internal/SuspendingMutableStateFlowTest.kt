ab050d59fd5d712d9d8e2e75b00c576e311d1c4c
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-01-14 23:10:58 +0100

    Add another test

--- core/src/test/kotlin/se/gustavkarlsson/conveyor/internal/SuspendingMutableStateFlowTest.kt
+++ core/src/test/kotlin/se/gustavkarlsson/conveyor/internal/SuspendingMutableStateFlowTest.kt
@@ -1,18 +1,18 @@
 package se.gustavkarlsson.conveyor.internal
 
-import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.withTimeout
-import org.junit.jupiter.api.fail
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import se.gustavkarlsson.conveyor.testing.memoizedTestCoroutineScope
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import strikt.assertions.isGreaterThan
+import strikt.assertions.isLessThan
 
 object SuspendingMutableStateFlowTest : Spek({
     val scope by memoizedTestCoroutineScope()
@@ -48,26 +48,22 @@ object SuspendingMutableStateFlowTest : Spek({
     describe("A SuspendingMutableStateFlow that does not suspend") {
         val subject by memoized { SuspendingMutableStateFlow(0, suspend = false) }
 
-        it("skips items using backpressure for slow collectors") {
-            val targetCount = 10
-            scope.runBlockingTest {
-                try {
-                    withTimeout(500) {
-                        val job = launch {
-                            subject
-                                .take(targetCount)
-                                .collect {
-                                    delay(100)
-                                }
-                        }
-                        repeat(targetCount) {
-                            subject.emit(it + 1)
-                        }
-                        job.cancel()
-                    }
-                } catch (e: TimeoutCancellationException) {
-                    fail(e)
+        it("always has the latest value even with slow collectors") {
+            var collectedValues = 0
+            runBlockingTest {
+                val job = launch {
+                    subject
+                        .onEach { delay(20) }
+                        .collect { collectedValues++ }
                 }
+                repeat(100) {
+                    subject.emit(it + 1)
+                    delay(10)
+                }
+                job.cancel()
+                expectThat(collectedValues)
+                    .isGreaterThan(10)
+                    .isLessThan(90)
             }
         }
     }
