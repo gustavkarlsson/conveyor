6b5ad8f508f4f9fa5608e03397c1bbfccabd9e24
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-01-16 15:42:22 +0100

    Update some versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,8 +2,8 @@ object Versions {
     // Config
     const val java = "1.8"
     const val kotlin = "1.4.20"
-    const val detekt = "1.14.2"
-    const val dokka = "1.4.10.2"
+    const val detekt = "1.15.0"
+    const val dokka = "1.4.20"
     const val jacoco = "0.8.6"
 
     // Dependencies
@@ -12,6 +12,6 @@ object Versions {
 
     // Test
     const val junit5 = "5.7.0"
-    const val spek = "2.0.14"
-    const val strikt = "0.28.0"
+    const val spek = "2.0.15"
+    const val strikt = "0.28.1"
 }
