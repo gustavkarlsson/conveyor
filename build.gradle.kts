930fd52d557779e776ca1eaa65ca694f73c7a335
Gustav Karlsson <gustav.karlsson@ioki.com>
2021-04-24 22:22:55 +0200

    Add binary-compatibility-validator

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("jvm") version Versions.kotlin apply false
     id("io.gitlab.arturbosch.detekt") version Versions.detekt apply false
     id("org.jetbrains.dokka") version Versions.dokka
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version Versions.binaryCompatibilityValidator apply false
 }
 
 repositories {
