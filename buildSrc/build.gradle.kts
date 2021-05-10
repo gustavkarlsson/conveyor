39ddc726760e0abfbefb15ab5a9aadb4ea151324
Gustav Karlsson <gustav.karlsson@ioki.com>
2021-05-10 19:50:01 +0200

    Update kotlin to 1.5.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 // FIXME duplicated versions in buildSrc/src/main/kotlin/Versions.kt
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
     implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.5.0")
     implementation("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:1.4.30")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.16.0")
