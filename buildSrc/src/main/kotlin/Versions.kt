93a7db9190bd72205b43e8f35a1b8b24eb622a5f
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-07-10 19:34:01 +0200

    Update kotlin to 1.5.20 and coroutines to 1.5.1 (#33)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,14 +1,14 @@
 // FIXME Duplicated versions in buildSrc/build.gradle.kts
 object Versions {
     // Config
-    const val kotlin = "1.5.10"
+    const val kotlin = "1.5.20"
     const val detekt = "1.17.1"
     const val dokka = "1.4.32"
     const val jacoco = "0.8.7"
     const val binaryCompatibilityValidator = "0.5.0"
 
     // Dependencies
-    const val kotlinxCoroutines = "1.5.0"
+    const val kotlinxCoroutines = "1.5.1"
 
     // Test
     const val junit5 = "5.7.2"
