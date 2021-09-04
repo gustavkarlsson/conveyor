a0bf22e1cabdb7771061d038212c6e460193bc9f
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-09-04 23:15:48 +0200

    Change ktlint rules and report in checkstyle format

--- buildSrc/src/main/kotlin/common.gradle.kts
+++ buildSrc/src/main/kotlin/common.gradle.kts
@@ -1,3 +1,11 @@
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 plugins {
     id("org.jlleitschuh.gradle.ktlint")
+}
+
+ktlint {
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+    }
 }
