dd39f6e990ee635f4cece6b65de6b9b9e3fc94e8
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-09-04 09:16:48 +0200

    Update all versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 
 // FIXME duplicated versions in buildSrc/src/main/kotlin/Versions.kt
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
-    implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.5.0")
-    implementation("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:1.4.32")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.17.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30")
+    implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.7.1")
+    implementation("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:1.5.0")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.18.1")
 }
