a518c8615bc4bf059720a0d645badc0064e80e12
Gustav Karlsson <gustav.karlsson@gmail.com>
2021-09-26 09:11:19 +0200

    Upgrade to sealed interfaces

--- plugin-vcr/src/main/kotlin/se/gustavkarlsson/conveyor/plugin/vcr/internal/Mode.kt
+++ plugin-vcr/src/main/kotlin/se/gustavkarlsson/conveyor/plugin/vcr/internal/Mode.kt
@@ -3,16 +3,16 @@ package se.gustavkarlsson.conveyor.plugin.vcr.internal
 import se.gustavkarlsson.conveyor.plugin.vcr.ReadableTape
 import se.gustavkarlsson.conveyor.plugin.vcr.WriteableTape
 
-internal sealed class Mode<out State> {
-    object Idle : Mode<Nothing>()
+internal sealed interface Mode<out State> {
+    object Idle : Mode<Nothing>
 
     data class Recording<State>(
         val writing: WriteableTape.Writing<State>,
         val bufferSize: Int,
-    ) : Mode<State>()
+    ) : Mode<State>
 
     data class Playing<State>(
         val reading: ReadableTape.Reading<State>,
         val bufferSize: Int,
-    ) : Mode<State>()
+    ) : Mode<State>
 }
