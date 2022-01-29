45a2ac63e46fa6275d32509853c3fae7211bf3bd
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-01-29 17:43:07 +0100

    Fix LiveActionTest

--- core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/actions/LiveActionTest.kt
+++ core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/actions/LiveActionTest.kt
@@ -1,7 +1,7 @@
 package se.gustavkarlsson.conveyor.actions
 
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -19,61 +19,61 @@ object LiveActionTest : Spek({
         val subject by memoized { TestLiveAction() }
 
         it("does not collect when store subscriber count is 0") {
-            runTest(UnconfinedTestDispatcher()) {
-                val launchJob = launch {
+            runTest {
+                launch {
                     subject.execute(flow)
                 }
                 flow.emit(1)
-                launchJob.cancel()
+                cancel()
             }
             expectThat(subject.collected).isEmpty()
         }
 
         it("collects current value when store subscribe count turns positive") {
-            runTest(UnconfinedTestDispatcher()) {
-                val launchJob = launch {
+            runTest {
+                launch {
                     subject.execute(flow)
                 }
                 flow.storeSubscriberCount.value = 1
-                launchJob.cancel()
+                cancel()
             }
             expectThat(subject.collected).containsExactly(initialValue)
         }
 
         it("does not collect current value again when store subscribe count changes between positive values") {
-            runTest(UnconfinedTestDispatcher()) {
-                val launchJob = launch {
+            runTest {
+                launch {
                     subject.execute(flow)
                 }
                 flow.storeSubscriberCount.value = 1
                 flow.storeSubscriberCount.value = 2
-                launchJob.cancel()
+                cancel()
             }
             expectThat(subject.collected).containsExactly(initialValue)
         }
 
         it("collects current value again when store subscribe count changes between positive and 0") {
-            runTest(UnconfinedTestDispatcher()) {
-                val launchJob = launch {
+            runTest {
+                launch {
                     subject.execute(flow)
                 }
                 flow.storeSubscriberCount.value = 1
                 flow.storeSubscriberCount.value = 0
                 flow.storeSubscriberCount.value = 1
-                launchJob.cancel()
+                cancel()
             }
             expectThat(subject.collected).containsExactly(initialValue, initialValue)
         }
 
         it("collects subsequent values when store subscribe count is positive") {
-            runTest(UnconfinedTestDispatcher()) {
-                val launchJob = launch {
+            runTest {
+                launch {
                     subject.execute(flow)
                 }
                 flow.storeSubscriberCount.value = 1
                 flow.emit(1)
                 flow.emit(2)
-                launchJob.cancel()
+                cancel()
             }
             expectThat(subject.collected).containsExactly(initialValue, 1, 2)
         }
