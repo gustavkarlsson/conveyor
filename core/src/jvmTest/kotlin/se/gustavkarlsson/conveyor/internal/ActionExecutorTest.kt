7d07ceb3a5fdd2e38f530349a2b1d382211faee8
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-02-02 19:18:52 +0100

    Migrate ActionExecutorTest to kotest

--- core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/internal/ActionExecutorTest.kt
+++ core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/internal/ActionExecutorTest.kt
@@ -1,16 +1,15 @@
 package se.gustavkarlsson.conveyor.internal
 
-import kotlinx.coroutines.cancel
+import io.kotest.core.spec.style.FunSpec
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
 import se.gustavkarlsson.conveyor.Action
 import se.gustavkarlsson.conveyor.testing.IncrementingAction
 import se.gustavkarlsson.conveyor.testing.SimpleStoreFlow
@@ -18,125 +17,105 @@ import strikt.api.expectThat
 import strikt.api.expectThrows
 import strikt.assertions.isEqualTo
 
-object ActionExecutorTest : Spek({
+// TODO Split subjects into separate files?
+class ActionExecutorTest : FunSpec({
     val initialState = 0
-    val actions by memoized { MutableSharedFlow<Action<Int>>() }
-    val storeFlow by memoized { SimpleStoreFlow(initialState) }
+    val actions = MutableSharedFlow<Action<Int>>()
+    val minimalStoreFlow = SimpleStoreFlow(initialState)
+    val minimalSubject = ActionExecutor(
+        startActions = emptyList(),
+        actions = actions,
+        transformers = emptyList(),
+        storeFlow = minimalStoreFlow,
+    )
 
-    describe("A minimal ActionExecutor") {
-        val subject by memoized {
-            ActionExecutor(
-                startActions = emptyList(),
-                actions = actions,
-                transformers = emptyList(),
-                storeFlow = storeFlow,
-            )
+    val twoActionStoreFlow = SimpleStoreFlow(initialState)
+    val startAction1 = IncrementingAction(1)
+    val startAction2 = IncrementingAction(2)
+    val twoActionSubject = ActionExecutor(
+        startActions = listOf(startAction1, startAction2),
+        actions = actions,
+        transformers = emptyList(),
+        storeFlow = twoActionStoreFlow,
+    )
+
+    val transformerStoreFlow = SimpleStoreFlow(initialState)
+    val transformer1 = { flow: Flow<Action<Int>> ->
+        flow.flatMapConcat { action ->
+            flowOf(action, action)
         }
+    }
+    val transformer2 = { flow: Flow<Action<Int>> ->
+        flow.drop(1)
+    }
+    val transformerSubject = ActionExecutor(
+        startActions = listOf(startAction1),
+        actions = actions,
+        transformers = listOf(transformer1, transformer2),
+        storeFlow = transformerStoreFlow,
+    )
 
-        it("does not execute action before run") {
+    test("does not execute action before run") {
+        runTest {
+            actions.emit(IncrementingAction(1))
+        }
+        expectThat(minimalStoreFlow.value).isEqualTo(0)
+    }
+
+    test("running executes emitted action") {
+        runTest {
+            val runJob = launch { minimalSubject.run() }
+            runCurrent()
+            actions.emit(IncrementingAction(1))
+            runCurrent()
+            expectThat(minimalStoreFlow.value).isEqualTo(1)
+            runJob.cancel()
+        }
+    }
+
+    test("running executes actions in parallel") {
+        runTest {
+            val runJob = launch { minimalSubject.run() }
+            runCurrent()
+            actions.emit(IncrementingAction(1, 100))
+            actions.emit(IncrementingAction(1, 100))
+            advanceTimeBy(100)
+            runCurrent()
+            expectThat(minimalStoreFlow.value).isEqualTo(2)
+            runJob.cancel()
+        }
+    }
+
+    test("throws if run a second time") {
+        expectThrows<IllegalStateException> {
             runTest {
-                actions.emit(IncrementingAction(1))
-            }
-            expectThat(storeFlow.value).isEqualTo(0)
-        }
-        describe("that was run") {
-            val runTest: (testBody: suspend TestScope.() -> Unit) -> Unit by memoized {
-                { testBody ->
-                    runTest {
-                        launch { subject.run() }
-                        testBody()
-                    }
-                }
-            }
-
-            it("executes action") {
-                runTest {
-                    launch { subject.run() }
-                    actions.emit(IncrementingAction(1))
-                    cancel()
-                }
-                expectThat(storeFlow.value).isEqualTo(1)
-            }
-            it("executes actions in parallel") {
-                runTest {
-                    actions.emit(IncrementingAction(1, 100))
-                    actions.emit(IncrementingAction(1, 100))
-                    // FIXME why does this test succeed without advancing time?
-                    //  advanceTimeBy(100)
-                    //  runCurrent()
-                    cancel()
-                }
-                expectThat(storeFlow.value).isEqualTo(2)
-            }
-            it("throws if run again") {
-                expectThrows<IllegalStateException> {
-                    runTest {
-                        subject.run()
-                    }
-                }
+                launch { minimalSubject.run() }
+                minimalSubject.run()
             }
         }
     }
-    describe("An ActionExecutor with 2 start actions") {
-        val startAction1 = IncrementingAction(1)
-        val startAction2 = IncrementingAction(2)
-        val subject by memoized {
-            ActionExecutor(
-                startActions = listOf(startAction1, startAction2),
-                actions = actions,
-                transformers = emptyList(),
-                storeFlow = storeFlow,
-            )
-        }
 
-        it("doesn't execute start actions before run") {
-            expectThat(storeFlow.value).isEqualTo(0)
-        }
+    test("doesn't execute start actions before run") {
+        expectThat(twoActionStoreFlow.value).isEqualTo(0)
+    }
 
-        describe("that was run") {
-            val runTest: (testBody: suspend TestScope.() -> Unit) -> Unit by memoized {
-                { testBody ->
-                    runTest {
-                        launch { subject.run() }
-                        testBody()
-                    }
-                }
-            }
-
-            it("executed start actions") {
-                runTest {
-                    cancel()
-                }
-                expectThat(storeFlow.value).isEqualTo(3)
-            }
+    test("executes start actions when run") {
+        runTest {
+            val runJob = launch { twoActionSubject.run() }
+            runCurrent()
+            expectThat(twoActionStoreFlow.value).isEqualTo(3)
+            runJob.cancel()
         }
     }
-    describe("An ActionExecutor with 2 transformers that was run") {
-        val startAction = IncrementingAction(1)
-        val transformer1 = { flow: Flow<Action<Int>> ->
-            flow.flatMapConcat { action ->
-                flowOf(action, action)
-            }
-        }
-        val transformer2 = { flow: Flow<Action<Int>> ->
-            flow.drop(1)
-        }
-        val subject by memoized {
-            ActionExecutor(
-                startActions = listOf(startAction),
-                actions = actions,
-                transformers = listOf(transformer1, transformer2),
-                storeFlow = storeFlow,
-            )
-        }
 
-        it("properly transforms actions") {
-            runTest {
-                launch { subject.run() }
-                actions.emit(IncrementingAction(2))
-                cancel()
-            }
-            expectThat(storeFlow.value).isEqualTo(5)
+    test("properly transforms actions") {
+        runTest {
+            val runJob = launch { transformerSubject.run() }
+            runCurrent()
+            actions.emit(IncrementingAction(2))
+            runCurrent()
+            expectThat(transformerStoreFlow.value).isEqualTo(5)
+            runJob.cancel()
         }
     }
 })
