1ba3e6db723af3f382e6e96696605df587c6e855
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-02-03 20:02:01 +0100

    Migrate PluginTest to kotest

--- core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/PluginTest.kt
+++ core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/PluginTest.kt
@@ -1,6 +1,6 @@
 package se.gustavkarlsson.conveyor
 
-import kotlinx.coroutines.cancel
+import io.kotest.core.spec.style.FunSpec
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.flow.flatMapConcat
@@ -9,15 +9,13 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
 import strikt.api.expect
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
-object PluginTest : Spek({
+class PluginTest : FunSpec({
 
-    describe("A store with overridden initial state") {
+    test("store with overridden initial state has expected initial state") {
         val plugin1 = object : Plugin<Int> {
             override fun overrideInitialState(initialState: Int) =
                 initialState + 2
@@ -26,16 +24,13 @@ object PluginTest : Spek({
             override fun overrideInitialState(initialState: Int) =
                 initialState * 2
         }
-        val store by memoized {
-            Store(1, plugins = listOf(plugin1, plugin2))
-        }
+        val store = Store(1, plugins = listOf(plugin1, plugin2))
 
-        it("has expected initial state") {
-            val result = store.state.value
-            expectThat(result).isEqualTo(6)
-        }
+        val result = store.state.value
+        expectThat(result).isEqualTo(6)
     }
-    describe("A store with added start actions") {
+
+    test("store with added start action shas expected state after starting") {
         val plugin1 = object : Plugin<Int> {
             override fun addStartActions(): Iterable<Action<Int>> {
                 return listOf(
@@ -54,21 +49,18 @@ object PluginTest : Spek({
                 )
             }
         }
-        val store by memoized {
-            Store(1, plugins = listOf(plugin1, plugin2))
-        }
+        val store = Store(1, plugins = listOf(plugin1, plugin2))
 
-        it("has expected state after starting") {
-            runTest {
-                launch { store.run() }
-                runCurrent()
-                val result = store.state.value
-                expectThat(result).isEqualTo(6)
-                cancel()
-            }
+        runTest {
+            val runJob = launch { store.run() }
+            runCurrent()
+            val result = store.state.value
+            expectThat(result).isEqualTo(6)
+            runJob.cancel()
         }
     }
-    describe("A store with transformed actions") {
+
+    test("store with transformed actions has expected state after issuing actions") {
         val plugin1 = object : Plugin<Int> {
             override fun transformActions(actions: Flow<Action<Int>>): Flow<Action<Int>> {
                 return actions.flatMapConcat { flowOf(it, it) }
@@ -79,28 +71,25 @@ object PluginTest : Spek({
                 return actions.drop(1)
             }
         }
-        val store by memoized {
-            Store(1, plugins = listOf(plugin1, plugin2))
-        }
+        val store = Store(1, plugins = listOf(plugin1, plugin2))
 
-        it("has expected state after issuing actions") {
-            runTest {
-                launch { store.run() }
-                runCurrent()
-                store.issue { storeFlow ->
-                    storeFlow.update { it + 2 }
-                }
-                store.issue { storeFlow ->
-                    storeFlow.update { it * 2 }
-                }
-                runCurrent()
-                val result = store.state.value
-                expectThat(result).isEqualTo(12)
-                cancel()
+        runTest {
+            val runJob = launch { store.run() }
+            runCurrent()
+            store.issue { storeFlow ->
+                storeFlow.update { it + 2 }
             }
+            store.issue { storeFlow ->
+                storeFlow.update { it * 2 }
+            }
+            runCurrent()
+            val result = store.state.value
+            expectThat(result).isEqualTo(12)
+            runJob.cancel()
         }
     }
-    describe("A store with transformed state") {
+
+    test("store with transformed state has expected state after issuing actions") {
         val plugin1 = object : Plugin<Int> {
             override fun transformStates(states: Flow<Int>): Flow<Int> {
                 return states.map { it + 2 }
@@ -111,26 +100,22 @@ object PluginTest : Spek({
                 return states.map { it * 2 }
             }
         }
-        val store by memoized {
-            Store(1, plugins = listOf(plugin1, plugin2))
-        }
+        val store = Store(1, plugins = listOf(plugin1, plugin2))
 
-        it("has expected state after issuing actions") {
-            runTest {
-                launch { store.run() }
-                runCurrent()
-                val initialState = store.state.value
-                store.issue { storeFlow ->
-                    storeFlow.update { it + 2 }
-                }
-                runCurrent()
-                val updatedState = store.state.value
-                expect {
-                    that(initialState).isEqualTo(6)
-                    that(updatedState).isEqualTo(10)
-                }
-                cancel()
+        runTest {
+            val runJob = launch { store.run() }
+            runCurrent()
+            val initialState = store.state.value
+            store.issue { storeFlow ->
+                storeFlow.update { it + 2 }
             }
+            runCurrent()
+            val updatedState = store.state.value
+            expect {
+                that(initialState).isEqualTo(6)
+                that(updatedState).isEqualTo(10)
+            }
+            runJob.cancel()
         }
     }
 })
