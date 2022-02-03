53fcaa23275e9c65d67e4928d5fd8a464bc4efc0
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-02-03 18:33:54 +0100

    Migrate TransformTest to kotest

--- core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/internal/TransformTest.kt
+++ core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/internal/TransformTest.kt
@@ -1,37 +1,35 @@
 package se.gustavkarlsson.conveyor.internal
 
+import io.kotest.core.spec.style.FunSpec
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
 import strikt.api.expectThat
 import strikt.assertions.containsExactly
 
-object TransformTest : Spek({
-    describe("A flow") {
-        val flow = flowOf(1, 2, 3)
+class TransformTest : FunSpec({
+    val flow = flowOf(1, 2, 3)
 
-        it("returns same flow if transforming with 0 transformers") {
-            runTest {
-                val result = flow.transform(emptyList()).toList()
-                expectThat(result).containsExactly(1, 2, 3)
-            }
+    test("returns same flow if transforming with 0 transformers") {
+        runTest {
+            val result = flow.transform(emptyList()).toList()
+            expectThat(result).containsExactly(1, 2, 3)
         }
-        it("returns transformed flow after multiple transformations") {
-            val doubleEach = { flow: Flow<Int> ->
-                flow.map { item -> item * 2 }
-            }
-            val filterOverThree = { flow: Flow<Int> ->
-                flow.filter { item -> item > 3 }
-            }
-            runTest {
-                val result = flow.transform(listOf(doubleEach, filterOverThree)).toList()
-                expectThat(result).containsExactly(4, 6)
-            }
+    }
+
+    test("returns transformed flow after multiple transformations") {
+        val doubleEach = { flow: Flow<Int> ->
+            flow.map { item -> item * 2 }
+        }
+        val filterOverThree = { flow: Flow<Int> ->
+            flow.filter { item -> item > 3 }
+        }
+        runTest {
+            val result = flow.transform(listOf(doubleEach, filterOverThree)).toList()
+            expectThat(result).containsExactly(4, 6)
         }
     }
 })
