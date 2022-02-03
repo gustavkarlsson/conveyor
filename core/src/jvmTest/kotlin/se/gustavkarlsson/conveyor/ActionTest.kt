e31a0e1d8fd65d54cf56787e3f3efe014053db4b
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-02-03 18:35:43 +0100

    Migrate ActionTest to kotest

--- core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/ActionTest.kt
+++ core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/ActionTest.kt
@@ -1,23 +1,19 @@
 package se.gustavkarlsson.conveyor
 
+import io.kotest.core.spec.style.FunSpec
 import kotlinx.coroutines.test.runTest
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
 import se.gustavkarlsson.conveyor.testing.IncrementingAction
 import se.gustavkarlsson.conveyor.testing.SimpleStoreFlow
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
-object ActionTest : Spek({
-    val storeFlow by memoized { SimpleStoreFlow(0) }
+class ActionTest : FunSpec({
+    val storeFlow = SimpleStoreFlow(0)
+    val subject = IncrementingAction(1)
 
-    describe("An action created with lambda") {
-        val subject by memoized { IncrementingAction(1) }
-
-        it("runs when execute") {
-            runTest {
-                subject.execute(storeFlow)
-            }
+    test("runs when execute") {
+        runTest {
+            subject.execute(storeFlow)
             expectThat(storeFlow.value).isEqualTo(1)
         }
     }
