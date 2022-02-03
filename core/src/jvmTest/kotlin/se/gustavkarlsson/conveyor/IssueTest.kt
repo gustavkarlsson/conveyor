306dc1e2fad9206c2ca29fd588c5887aefa5b068
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-02-03 18:37:21 +0100

    Migrate IssueTest to kotest

--- core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/IssueTest.kt
+++ core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/IssueTest.kt
@@ -1,31 +1,25 @@
 package se.gustavkarlsson.conveyor
 
-import kotlinx.coroutines.cancel
+import io.kotest.core.spec.style.FunSpec
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
 
-object IssueTest : Spek({
-    describe("A store") {
-        val store by memoized {
-            Store(0)
-        }
+class IssueTest : FunSpec({
+    val store = Store(0)
 
-        it("after started, issue extension function executes body as expected") {
-            runTest {
-                launch { store.run() }
-                runCurrent()
-                store.issue { storeFlow ->
-                    storeFlow.update { it + 1 }
-                }
-                runCurrent()
-                expectThat(store.state.value).isEqualTo(1)
-                cancel()
+    test("after started, issue extension function executes body as expected") {
+        runTest {
+            val runJob = launch { store.run() }
+            runCurrent()
+            store.issue { storeFlow ->
+                storeFlow.update { it + 1 }
             }
+            runCurrent()
+            expectThat(store.state.value).isEqualTo(1)
+            runJob.cancel()
         }
     }
 })
