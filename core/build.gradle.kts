5424fad4d2c98b82556594270890d5244639a88a
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-02-05 18:50:10 +0100

    Load kotest libs in commonTest

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,14 +16,15 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(project(":test", "archives"))
+                implementation(libs.kotest.assertions)
+                implementation("io.kotest:kotest-framework-engine:5.1.0") // FIXME catalog
             }
         }
         val jvmTest by getting {
             dependencies {
                 runtimeOnly(libs.bundles.testRuntime)
                 implementation(libs.bundles.testImplementation)
-                implementation(libs.kotest)
-                implementation(libs.kotest.assertions)
+                implementation("io.kotest:kotest-runner-junit5-jvm:5.1.0")
             }
         }
         val jsTest by getting
