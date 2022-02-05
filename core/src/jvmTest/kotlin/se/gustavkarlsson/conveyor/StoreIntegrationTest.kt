341a2347779f4109eddf2af5573d5da9729295b1
Gustav Karlsson <gustav.karlsson@gmail.com>
2022-02-05 11:40:52 +0100

    Migrate StoreIntegrationTest to kotest

--- core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/StoreIntegrationTest.kt
+++ core/src/jvmTest/kotlin/se/gustavkarlsson/conveyor/StoreIntegrationTest.kt
@@ -1,14 +1,10 @@
 package se.gustavkarlsson.conveyor
 
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
+import io.kotest.core.spec.style.FunSpec
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
-import org.spekframework.spek2.Spek
-import org.spekframework.spek2.style.specification.describe
 import se.gustavkarlsson.conveyor.testing.NullAction
 import se.gustavkarlsson.conveyor.testing.SetStateAction
 import strikt.api.expectThat
@@ -16,133 +12,128 @@ import strikt.api.expectThrows
 import strikt.assertions.isEqualTo
 import strikt.assertions.isTrue
 
-object StoreIntegrationTest : Spek({
+class StoreIntegrationTest : FunSpec({
     val initialState = "initial"
     val state1 = "state1"
     val fixedStateAction1 = SetStateAction(state1)
     val action = NullAction<String>()
 
-    describe("A minimal store") {
-        val subject by memoized {
-            Store(initialState)
-        }
+    val minimalStore = Store(initialState)
 
-        it("state emits initial") {
-            runTest {
-                val result = subject.state.first()
-                expectThat(result).isEqualTo(initialState)
-                cancel()
+    val failingAction = Action<String> { error("failed") }
+    val storeWithFailingAction = Store(initialState, startActions = listOf(failingAction))
+
+    val failingWhileUpdatingAction = Action<String> { storeFlow -> storeFlow.update { error("failed") } }
+    val storeWithFailingWhileUpdatingAction = Store(initialState, startActions = listOf(failingWhileUpdatingAction))
+
+
+    test("state emits initial") {
+        runTest {
+            val result = minimalStore.state.first()
+            runCurrent()
+            expectThat(result).isEqualTo(initialState)
+
+        }
+    }
+
+    test("state.value returns initial") {
+        val result = minimalStore.state.value
+        expectThat(result).isEqualTo(initialState)
+    }
+
+    test("throws when issuing action") {
+        expectThrows<StoreNotYetStartedException> {
+            minimalStore.issue(fixedStateAction1)
+        }
+    }
+
+
+
+
+    test("A store with a starting action that fails when updating cancels when started") {
+        expectThrows<IllegalStateException> {
+            storeWithFailingWhileUpdatingAction.run()
+        }
+    }
+
+    test("A store with a starting action that fails when executed cancels when started") {
+        expectThrows<IllegalStateException> {
+            storeWithFailingAction.run()
+        }
+    }
+
+
+    test("started store has an active job") {
+        runTest {
+            val runJob = launch { minimalStore.run() }
+            expectThat(runJob.isActive).isTrue()
+            runJob.cancel()
+        }
+    }
+
+    test("started store throws exception when started") {
+        runTest {
+            val runJob = launch { minimalStore.run() }
+            runCurrent()
+            expectThrows<StoreAlreadyStartedException> {
+                minimalStore.run()
+            }
+            runJob.cancel()
+        }
+    }
+
+    test("started store state emits initial") {
+        runTest {
+            val runJob = launch { minimalStore.run() }
+            runCurrent()
+            val result = minimalStore.state.first()
+            expectThat(result).isEqualTo(initialState)
+            runJob.cancel()
+        }
+    }
+
+    test("a store that was started and then cancelled throws exception when started") {
+        runTest {
+            val runJob = launch { minimalStore.run() }
+            runCurrent()
+            runJob.cancel()
+            runCurrent()
+            expectThrows<StoreStoppedException> {
+                minimalStore.run()
             }
         }
-        it("state.value returns initial") {
-            val result = subject.state.value
+    }
+    test("a store that was started and then cancelled throws exception when an action is issued") {
+        runTest {
+            val runJob = launch { minimalStore.run() }
+            runCurrent()
+            runJob.cancel()
+            runCurrent()
+            expectThrows<StoreStoppedException> {
+                minimalStore.issue(action)
+            }
+        }
+    }
+
+    test("a store that was started and then cancelled, state.value returns initial") {
+        runTest {
+            val runJob = launch { minimalStore.run() }
+            runJob.cancel()
+            runCurrent()
+            val result = minimalStore.state.value
             expectThat(result).isEqualTo(initialState)
         }
-        it("throws when issuing action") {
-            expectThrows<StoreNotYetStartedException> {
-                subject.issue(fixedStateAction1)
-            }
-        }
+    }
 
-        describe("that was started") {
-            val runTest: (testBody: suspend TestScope.(Job) -> Unit) -> Unit by memoized {
-                { testBody ->
-                    runTest {
-                        val job = launch { subject.run() }
-                        testBody(job)
-                    }
-                }
-            }
-
-            it("has an active job") {
-                runTest { job ->
-                    expectThat(job.isActive).isTrue()
-                    cancel()
-                }
-            }
-            /*
-            FIXME can't get this to run
-            it("throws exception when started") {
-                runTest {
-                    runCurrent()
-                    expectThrows<StoreAlreadyStartedException> {
-                        launch { subject.run() }
-                        runCurrent()
-                    }
-                    cancel()
-                }
-            }
-            */
-            it("state emits initial") {
-                runTest {
-                    runCurrent()
-                    val result = subject.state.first()
-                    expectThat(result).isEqualTo(initialState)
-                    cancel()
-                }
-            }
-
-            describe("and had its job explicitly cancelled") {
-                val runTest: (testBody: suspend TestScope.(Job) -> Unit) -> Unit by memoized {
-                    { testBody ->
-                        runTest { job ->
-                            job.cancel("Purposefully cancelled before test")
-                            testBody(job)
-                        }
-                    }
-                }
-                /*
-                FIXME can't get this to run
-                it("throws exception when started") {
-                    expectThrows<StoreStoppedException> {
-                        subject.run()
-                    }
-                }
-                it("throws exception when an action is issued") {
-                    expectThrows<StoreStoppedException> {
-                        subject.issue(action)
-                    }
-                }
-                */
-                it("state.value returns initial") {
-                    runTest {
-                        val result = subject.state.value
-                        expectThat(result).isEqualTo(initialState)
-                    }
-                }
-                it("state emits initial") {
-                    runTest {
-                        val result = subject.state.first()
-                        expectThat(result).isEqualTo(initialState)
-                    }
-                }
-            }
+    test("a store that was started and then cancelled, state emits initial") {
+        runTest {
+            val runJob = launch { minimalStore.run() }
+            runCurrent()
+            runJob.cancel()
+            runCurrent()
+            val result = minimalStore.state.first()
+            expectThat(result).isEqualTo(initialState)
         }
     }
-    describe("A store with a starting action that fails when executed") {
-        val failingAction = Action<String> { error("failed") }
-        val subject by memoized {
-            Store(initialState, startActions = listOf(failingAction))
-        }
 
-        it("cancels when started") {
-            expectThrows<IllegalStateException> {
-                subject.run()
-            }
-        }
-    }
-    describe("A store with a starting action that fails when updating") {
-        val failingAction = Action<String> { storeFlow ->
-            storeFlow.update { error("failed") }
-        }
-        val subject by memoized {
-            Store(initialState, startActions = listOf(failingAction))
-        }
-
-        it("cancels when started") {
-            expectThrows<IllegalStateException> {
-                subject.run()
-            }
-        }
-    }
 })
