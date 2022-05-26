07f969215f3c57a3abd5fc02abfc5a2446d7e014
Gustav Karlsson <gustav.karlsson@epidemicsound.com>
2022-05-26 10:27:47 +0200

    Upgrade versions and swap jacoco for kover

--- buildSrc/src/main/kotlin/published.gradle.kts
+++ buildSrc/src/main/kotlin/published.gradle.kts
@@ -3,11 +3,7 @@ plugins {
     id("org.jetbrains.kotlinx.binary-compatibility-validator")
     id("org.jetbrains.dokka")
     id("io.kotest.multiplatform")
-    // java is required for jacoco according to
-    // https://nwillc.medium.com/kotlin-multiplatform-first-contact-bintray-jacoco-part-3-dbd496bf168a
-    java
     `maven-publish`
-    jacoco
 }
 
 repositories {
@@ -66,19 +62,6 @@ publishing {
             // artifact(tasks["javadocJar"]) TODO re-add once dokka supports multiplatform
             artifact(tasks["jvmJar"])
         }
-    }
-}
-
-tasks.jacocoTestReport {
-    dependsOn("jvmTest")
-    val sourceDirs = arrayOf("commonMain/src", "jvmMain/src")
-    sourceDirectories.setFrom(sourceDirs)
-    additionalSourceDirs.setFrom(sourceDirs)
-    classDirectories.setFrom("${buildDir}/classes/kotlin/jvm/main/")
-    executionData.setFrom("${buildDir}/jacoco/jvmTest.exec")
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
     }
 }
 
